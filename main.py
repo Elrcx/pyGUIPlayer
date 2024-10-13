8b7c5b97dee70b4c4d12076e93caa71d05d1c3e9
Elrcx <61328598+Elrcx@users.noreply.github.com>
2024-10-13 02:05:41 +0200

    added buttons

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ app = ttk.Window(themename="darkly")
 app.title("Music Player")
 app.geometry("400x300")
 
+load_button = ttk.Button(app, text="Play from file")
+load_button.pack(pady=10)
+
+pause_button = ttk.Button(app, text="Pause")
+pause_button.pack(pady=10)
+
+unpause_button = ttk.Button(app, text="Unpause")
+unpause_button.pack(pady=10)
+
+stop_button = ttk.Button(app, text="Stop")
+stop_button.pack(pady=10)
+
 app.mainloop()
